936e65b1fcd5eab0ec18fc8ef8e54d05c89a35f0
Erock FaQue <eric.kemerer@gmail.com>
2022-01-23 15:10:10 -0500

    start button initializes functions, 1st question appears

---
+++ assets/javascript/script.js
@@ -0,0 +1,44 @@
+const startButton = document.getElementById('start-btn')
+const questionContainerElement = document.getElementById
+('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonElement = document.getElementById('answer-buttons')
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame)
+
+function startGame() {
+    //console.log(started)
+    startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
+}
+
+function setNextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+
+}
+
+function showQuestion(question){
+    questionElement.innerText = question.question
+
+}
+
+function selectAnswer() {
+
+}
+
+const questions = [
+    {
+        question: 'What is 7 x 6?',
+        answers: [
+            { text: '42', correct: true},
+            { text: '32', correct: false},
+            { text: '76', correct: false},
+            { text: '54', correct: false}
+        ]
+    }
+]
+
